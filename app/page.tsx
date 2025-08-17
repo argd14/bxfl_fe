147fd7af5c821fea3695b11b8b941376603c725c
Andres Garcia <argd14@gmail.com>
2025-08-16 23:05:29 -0600

    login screen and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { Form, Input, Button, Typography } from 'antd'
+import Link from 'next/link'
+import styles from './login/login.module.css'
+import { useRouter } from 'next/navigation'
 
+const { Title, Text } = Typography
 export default function Home() {
+  
+  const router = useRouter()
+ const handleRegister = () => {
+      router.push('./register')
+    }
+  const onFinish = (values: any) => {
+    console.log('Received values:', values)
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+      <div className={styles.formSection}>
+        <div className={styles.formWrapper}>
+          <Title level={3}>Bienvenido</Title>
+          <Text>Por favor ingresa tus credenciales</Text>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <Form layout="vertical" onFinish={onFinish} className={styles.form}>
+            <Form.Item
+              label="Correo electrónico"
+              name="email"
+              rules={[{ required: true, message: 'Por favor ingresa tu correo' }]}
+            >
+              <Input placeholder="Digita tu correo" />
+            </Form.Item>
+
+            <Form.Item
+              label="Contraseña"
+              name="password"
+              rules={[{ required: true, message: 'Por favor ingresa tu contraseña' }]}
+            >
+              <Input.Password placeholder="Digita el NIT del comercio" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                Iniciar sesión
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <Text type="secondary" className={styles.registerText} >
+            ¿Necesitas una cuenta? <Link onClick={handleRegister} href="/register">Regístrate aquí</Link>
+          </Text>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className={styles.blankSection}></div>
     </div>
   );
 }
